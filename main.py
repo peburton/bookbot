5c0a56665192ab4c5cc525fda70737e199ece23a
peburton <113070810+peburton@users.noreply.github.com>
2024-06-08 14:40:11 -0600

    collapsed two functions together for efficiency and readability -- funtion which lowered the case of the text and counted the letters

--- main.py
+++ main.py
@@ -6,22 +6,18 @@ def word_counter(file_contents):
 	word_count = file_contents.split()
 	return len(word_count)
 
-def lower_case(file_contents):
-	lowered_string = file_contents.lower()
-	return lowered_string
-
-def count_letters(lowered_string):
+def count_letters(file_contents):
 	freq = {}
-	for x in set(lowered_string):
-		freq[x] = lowered_string.count(x)
+	lowered = file_contents.lower()
+	for x in set(lowered):
+		freq[x] = lowered.count(x)
 	return freq
 
 def main():
 	file_path = "books/frankenstein.txt"
 	file_contents = read_text(file_path)
 	word_count = word_counter(file_contents)
-	lowered_string = lower_case(file_contents)
-	letter_freq = count_letters(lowered_string)
+	letter_freq = count_letters(file_contents)
 	print(file_contents)
 	print (f'The number of words in this file is: {word_count}')
 	print (f'The number of times each character appears in the text is:{letter_freq}')
