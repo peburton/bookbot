7b484bda9b4af59e4704c84492192ab5d76bf36a
peburton <113070810+peburton@users.noreply.github.com>
2024-06-08 14:05:02 -0600

    added counter script

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+def read_text(file_path):
+	with open(file_path) as f:
+		return f.read()
+
+def word_counter(file_contents):
+	word_count = file_contents.split()
+	return len(word_count)
+
 def main():
-	with open("books/frankenstein.txt") as f:
-		file_contents = f.read()
-		print(file_contents)
+	file_path = "books/frankenstein.txt"
+	file_contents = read_text(file_path)
+	word_count = word_counter(file_contents)
+	print (f'The number of words in this file is: {word_count}')
 
 if __name__ == "__main__":
     main()
