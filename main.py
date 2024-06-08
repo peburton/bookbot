7d2545f6ce7aa83352105f812d7730cf2218f4a1
peburton <113070810+peburton@users.noreply.github.com>
2024-06-08 15:12:26 -0600

    Develop final report printout -- take letter_freq dictionary, check key value pairs for alphabetic keys, print out key value pairs in report

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def main():
 	file_contents = read_text(file_path)
 	word_count = word_counter(file_contents)
 	letter_freq = count_letters(file_contents)
-	print(file_contents)
-	print (f'The number of words in this file is: {word_count}')
-	print (f'The number of times each character appears in the text is:{letter_freq}')
+	print(f'---Begin report of {file_path}---'+'\n'+'\n'+f'The number of words in this file is: {word_count}'+'\n')
+	for key, value in letter_freq.items():
+		if key.isalpha():
+			print(f'The {key} character was found {value} times')
 
 if __name__ == "__main__":
     main()
